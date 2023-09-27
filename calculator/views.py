d23a9d73ba0d30ec73fdcba210ea28a0a3e4987c
Mehdizamanian <104761166+Mehdizamanian@users.noreply.github.com>
2023-09-27 15:49:22 +0330

    logic completed

--- calculator/views.py
+++ calculator/views.py
@@ -1,28 +1,24 @@
 from django.shortcuts import render
 
 def index(request):
-    c=0
+    c=''
     try:
         if request.method=="POST":
-            n1=eval(request.POST.get("num1"))
-            n2=eval(request.POST.get("num1"))
-            opr=request.POST.get("opr")  
-            
-
+            n1=eval(request.POST.get('num1'))
+            n2=eval(request.POST.get('num2'))
+            opr=request.POST.get('opr')  
             if opr=="+":
-                c=n1+n2
-
+                c=n1+n2;
             elif opr=="-":
                 c=n1-n2
-
             elif opr=="*":
-                c=n1*n2   
-
+                c=n1*n2 ;  
             elif opr=="/":
                 c=n1/n2
+
     except:
       c="select your operator"
- 
+    print(c)
     return render(request,'calculator/index.html',{'c':c})
 
 # Create your views here.
