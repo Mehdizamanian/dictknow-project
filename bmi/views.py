780b6aba1e4cc6cd3be7200f32cca5c5c5070a43
Mehdizamanian <104761166+Mehdizamanian@users.noreply.github.com>
2023-10-01 21:31:31 +0330

    Bmi view completed

--- bmi/views.py
+++ bmi/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.contrib import messages
 
+def bmiView(request):
+    c=''
+    try:
+        if request.method=="POST":
+            weight=eval(request.POST.get('weight'))
+            age=eval(request.POST.get('age')) 
+            if weight<83 and age<23:
+                c='weight is normal'
+            # elif opr=="-":
+            #     c=n1-n2
+            # elif opr=="*":
+            #     c=n1*n2 ;  
+            # elif opr=="/":
+            #     c=n1/n2
 
-def bmiView(requet):
-    return render(requet,'bmi/bmi.html')
+    except:
+      c="numbers not found , please enter your numbers again"
+    print(c)
+    return render(request,'bmi/bmi.html',{"c":c})
 # Create your views here.
