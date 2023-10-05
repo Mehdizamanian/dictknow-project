78fe309fc56d98903e4c8ee9730bd4e6f51ade03
Mehdizamanian <104761166+Mehdizamanian@users.noreply.github.com>
2023-10-05 12:17:11 +0330

    we are at firstly position

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
     return render(request,'dictionary/index.html')
 
 def search(request):
     word = request.GET['word']
+    # res=request.get('https://www.dictionary.com/browse/order')
     return render(request,'dictionary/topics-listing.html',{'word':word})
 
 
