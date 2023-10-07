165a843c5752e43635f7e5e7e50fc1220df337bb
Mehdizamanian <104761166+Mehdizamanian@users.noreply.github.com>
2023-10-07 22:07:17 +0330

    completed view without sy and an

--- dictionary/views.py
+++ dictionary/views.py
@@ -6,9 +6,21 @@ def index(request):
     return render(request,'dictionary/index.html')
 
 def search(request):
-    word = request.GET['word']
-    # res=request.get('https://www.dictionary.com/browse/order')
-    return render(request,'dictionary/topics-listing.html',{'word':word})
+    word = request.GET.get('word')
+    dictionary=PyDictionary()
+    meaning =dictionary.meaning(word)
+    synonyms =dictionary.synonym(word)
+    antonyms = dictionary.antonym(word)
 
+    
+    context ={
+        'word':word , 
+        'meaning':meaning['Noun'][0],
+        'synonyms':synonyms,
+        'antonyms':antonyms,
+    }
 
+    return render(request,'dictionary/topics-listing.html',context)
+
+    print (dictionary.antonym("Life"))
 # Create your views here.
